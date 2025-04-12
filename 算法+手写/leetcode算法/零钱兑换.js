c7eba51099e5ff8849fe0bfc7777993d544bbf97
camoel <2048579509@qq.com>
2025-04-12 16:15:58 +0800

    feat: leetcode*1

---
+++ 算法+手写/leetcode算法/零钱兑换.js
@@ -0,0 +1,10 @@
+function coinChange(coin, amount) {
+  const dp=new Array(amount+1).fill(Infinity)
+  dp[0]=0
+  for(let i=1;i<=amount;i++){
+    if(i-coin>=0){
+      dp[i]=Math.min(dp[i],dp[i-coin]+1)
+    }
+  }
+  return dp[amount]===Infinity?-1:dp[amount]
+};
